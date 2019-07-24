87d96e9775a90ff0691899f9a71d62c962e4ad79
André Cristiano <andre.cristiano1975@gmail.com>
2019-07-24 16:03:03 -0300

    Commit initial

---
+++ main.js
@@ -0,0 +1,15 @@
+const { resolve } = require('path');
+const { app, Menu, Tray } = require('electron');
+
+// app.dock.hide();
+
+app.on('ready', () => {
+    const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: 'radio', checked: true }
+    ]);
+
+    tray.setToolTip('This is may application');
+    tray.setContextMenu(contextMenu);
+})
