39b81df0f9aed2e4c5659b466c1bc8a6a967101b
André Cristiano <andre.cristiano1975@gmail.com>
2019-07-25 07:00:43 -0300

    feat:Adcionados o electron-store e electron-reloader

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
-const { resolve } = require('path');
-const { app, Menu, Tray } = require('electron');
+const { resolve, basename } = require('path');
+const { app, Menu, Tray, dialog } = require('electron');
+const Store = require('electron-store');
+
+const schema = {
+    projects:{
+        type: 'array',
+
+    },
+};
+
+const store = new Store({ schema });
 
 // app.dock.hide();
+
+const storeProjects = store.get('projects');
+const projects = storeProjects ? JSON.parse(storeProjects) : [];
+const itens = projects.map((project) => {
+    return { label: project.name, click: () => spaw.sync('code', [project.path]) }
+});
 
 app.on('ready', () => {
     const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
 
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'Item1', type: 'radio', checked: true }
+        ...itens,
+        { 
+            label: 'Adicionar novo projeto ...', 
+            type: 'radio', 
+            click: () => {
+                const [path] = dialog.showOpenDialog({properties: ['openDirectory']});
+
+                store.set('projects', [JSON.stringify([...projects, {
+                    path,
+                    name: basename(path)
+                }])])
+                console.log(projects);
+            
+            } 
+        }
     ]);
 
     tray.setToolTip('This is may application');
